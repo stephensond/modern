06a54d7a4c2df0c5ef7af5546b98be5cc204bd85
Cheesywizz <70651459+Cheesywizz@users.noreply.github.com>
2021-06-21 18:06:27 -0400

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,20 +2,16 @@ import React from 'react'
 
 class Header extends React.Component<any, any> {
 
-    constructor(props: any) {
-        super(props);
-    }
-
     render () {
         return (
             <div className="Header">
                 <h1 className="title">Modern Fantasy</h1>
                 <ul className="headerList">
-                    <li className="headerItem"><a className="headerContent">Home</a></li>
-                    <li className="headerItem"><a className="headerContent">Leagues</a></li>
-                    <li className="headerItem"><a className="headerContent">Draft</a></li>
-                    <li className="headerItem"><a className="headerContent">About</a></li>
-                    <li className="headerItem"><a className="headerContent" onClick={this.props.onClick}>New Draft</a></li>
+                    <li className="headerItem"><button className="headerContent">Home</button></li>
+                    <li className="headerItem"><button className="headerContent">Leagues</button></li>
+                    <li className="headerItem"><button className="headerContent">Draft</button></li>
+                    <li className="headerItem"><button className="headerContent">About</button></li>
+                    <li className="headerItem"><button className="headerContent" onClick={this.props.onClick}>New Draft</button></li>
                 </ul>
             </div>
         )
