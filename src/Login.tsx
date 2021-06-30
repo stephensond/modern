dfac657bf374e7e9699a1b00bccdcc186cd65208
David Stephenson <stephenson.d@northeastern.edu>
2021-06-30 17:50:33 -0400

    add custom button

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,6 @@
-import React, { ReactElement , useState } from "react";
-
+import React, { ReactElement, useState } from "react";
 
 export default function Login(): ReactElement {
-
   const [username, setUsername] = useState("");
   const [pass, setPass] = useState("");
 
@@ -12,36 +10,42 @@ export default function Login(): ReactElement {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({'username' : username, 'pass' : pass}),
+      body: JSON.stringify({ username: username, pass: pass }),
     };
     fetch("https://modern-fantasy.herokuapp.com/addUser", requestOptions)
       .then((res) => res.status)
-      .then((status) => {if (status === 200) {alert('Account created')}
-                         else if (status === 400) {alert('Username taken')}})
+      .then((status) => {
+        if (status === 200) {
+          alert("Account created");
+        } else if (status === 400) {
+          alert("Username taken");
+        }
+      })
       .catch((error) => console.log(error));
-  }
+  };
 
   const updateUsername = (event) => {
-    setUsername(event.target.value)
-  }
+    setUsername(event.target.value);
+  };
 
   const updatePass = (event) => {
-    setPass(event.target.value)
-  }
+    setPass(event.target.value);
+  };
 
   return (
     <div className="Login">
-      <form onSubmit={addUser}>
-        <label>
-          Username:
-          <input type="text" onChange={updateUsername} />
-        </label>
-        <label>
-          Password:
-          <input type="text" onChange={updatePass} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <label>
+        Username:
+        <input type="text" onChange={updateUsername} />
+      </label>
+      <label>
+        Password:
+        <input type="text" onChange={updatePass} />
+      </label>
+      <input type="submit" value="Submit" />
+      <button onClick={addUser} className="createUser">
+        Submit
+      </button>
     </div>
   );
 }
