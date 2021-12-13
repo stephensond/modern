4564e94768715a4dcffca539504773f5679b3f50
David Stephenson <stephenson.d@northeastern.edu>
2021-12-12 21:14:25 -0500

    protect pages

--- common/header-unauthed/index.jsx
+++ common/header-unauthed/index.jsx
@@ -1,15 +1,8 @@
 import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
-import { UserContext } from '../../context/useUserContext';
+import React from 'react';
 import styles from './header-unauthed.module.css';
 
 export default function HeaderUnauthed({ children }) {
-  const { user } = useContext(UserContext);
-
-  if (user) {
-    return null;
-  }
-
   return (
     <div className={styles.container}>
       <p className={styles.title}>Welcome to Modern Fantasy!</p>
