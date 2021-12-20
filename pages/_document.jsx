067f69035c7f1926382c2ec8a640e2fc878fb2ed
David Stephenson <stephenson.d@northeastern.edu>
2021-12-20 09:31:14 -0500

    minor change

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -15,7 +15,6 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&amp;display=swap"
